812f7c3dae9f65812e2cf5889c5a19904f787db4
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2022-09-28 20:44:17 +0200

    Version opcional preparada para evaluar.

--- Tema01/Practica01-MyFirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Tema01/Practica01-MyFirstApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage(view);
+            }
+        });
     }
 
     /** Called when the user taps the Send button */
@@ -23,4 +32,5 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
 }
